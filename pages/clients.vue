6cfc1940abf422e913c0d9a3056c6945bd454699
mohamedSalah1994 <elkomymohamed69@gmail.com>
2021-01-04 04:23:19 +0200

    client api

--- pages/clients.vue
+++ pages/clients.vue
@@ -1,7 +1,18 @@
 <template>
   <div class="container">
     <div>
-      Clients
+      <h1>Clients</h1>
+      <Row>
+
+        <Card style="margin: 10px" v-for="row in rows">
+          <div style="text-align:center">
+            <img :src="'http://127.0.0.1:8000/storage/'+row.image" width="250">
+          </div>
+        </Card>
+
+      </Row>
+
+
     </div>
   </div>
 </template>
@@ -9,8 +20,29 @@
 <script>
 
 export default {
+  data() {
+    return {
+      rows: []
+    }
+  },
+  mounted() {
+    this.fetchData()
+  },
+  methods: {
+    fetchData() {
+
+      this.$axios.$get('http://127.0.0.1:8000/api/module/clients').then((res) => {
+              this.rows = res.payload.data;
+
+      })
+
+
+    }
+  }
+
 
 }
+
 </script>
 
 
